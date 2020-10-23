80cd099b355dd7c155e2d4a4f20b54115a2d27e9
Misel Tekinder <misel.tekinder@gmail.com>
2020-10-23 23:24:00 +0200

    SASS processing added

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,3 +1,26 @@
+const { src, dest } = require('gulp');
+
+const plumber = require('gulp-plumber');
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+
+// Setting up SCSS compiling
+function compileSCSS() {
+  return src('./src/scss/style.scss')
+    .pipe(plumber({
+      errorHandler: function(error) {
+        console.log(error);
+      }
+    }))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(dest('.'));
+};
+
+
 
 function gulpTask(cb) {
   console.log('👋 Hello!');
@@ -5,3 +28,4 @@ function gulpTask(cb) {
 };
 
 exports.default = gulpTask;
+exports.compile = compileSCSS;
