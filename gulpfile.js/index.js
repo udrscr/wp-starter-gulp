c55fd11cc3d0b9e3d3b03704897e632293792279
Misel Tekinder <misel.tekinder@gmail.com>
2020-11-21 11:37:43 +0100

    Build process connected

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,4 +1,4 @@
-const { src, dest, parallel } = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 
 // TODO: Add process for assets
 // TODO: Add Husky for hooks
@@ -9,32 +9,40 @@ const log = require('fancy-log');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
+// #####################################################################
+// Configs
+
+const fileSRC = {
+  jsFiles: './src/js/*.js',
+  jsSRC: './src/js/index.js',
+  scssFiles: './src/scss/*.scss',
+  scssSRC: './src/scss/style.scss'
+};
+
+// #####################################################################
+
 
 // Setting up SCSS processing
 function processSCSS() {
-  return src('./src/scss/style.scss')
+  return src(fileSRC.scssSRC)
     .pipe(plumber({ errorHandler: handleError }))
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(dest('.'))
     .on('end', function() {
-      console.log('SCSS compiled!');
+      log('Style.css created at location: "./style.css"');
     });
 };
 
 
 // // Setting up JS processing
-function compileJS() {
-  return src('./src/js/**/*.js')
+function processJS() {
+  return src(fileSRC.jsSRC)
     .pipe(plumber({ errorHandler: handleError }))
-    .pipe(dest('./js/'))
+    .pipe(dest('.'))
     .on('end', function() {
-      console.log('JS compiled!');
+      log('index.js created at location: "./index.js"');
     });
 };
-
-
-
-
 
 
 
@@ -45,8 +53,18 @@ function gulpTask(cb) {
   cb();
 };
 
+function dev() {
+  const settings = {
+    queue: true
+  };
+
+  watch(fileSRC.jsFiles, settings, processJS);
+  watch(fileSRC.scssFiles, settings, processSCSS);
+};
+
 exports.default = gulpTask;
-exports.compile = parallel(processSCSS, compileJS);
+exports.compile = parallel(processSCSS, processJS);
+exports.dev = dev;
 
 
 // #############################################################################
@@ -54,5 +72,5 @@ exports.compile = parallel(processSCSS, compileJS);
 // Helper functions
 
 function handleError(error) {
-  console.log(error);
+  log(error);
 };
---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+// Main JS file to be compiled
+// 
